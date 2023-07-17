1798f58ceae8d4b697a071ef80a548018afc1286
luke123adams <lxa207@gmail.com>
2023-07-17 12:54:50 +0100

    attempting to evolve db

--- index.ts
+++ index.ts
@@ -3,22 +3,16 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.first_names.create({
-        data: {
-            name: 'Bilbo',
-            gender: 'M',
-            race_id: 1
-        },
-    })
 
-    const baggins = await prisma.first_names.findFirst({
+    const Jamelia = await prisma.first_names.update({
         where: {
-            name : 'Bilbo'
+            name : 'Bilbo',
+        
         },
     })
 
 
-  console.log(baggins)
+  console.log(Jamelia)
 
 }
 
